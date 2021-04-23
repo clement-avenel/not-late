abb2740e132fbbaf2f3926e11c5bde3770c3bda4
Clement <contact@clement-avenel.com>
2021-04-23 20:04:02 +0200

    404 error fix

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/', apiRoutes);
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-app.use((req, res, next) => {
+app.use('*', (req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
   next(error);
